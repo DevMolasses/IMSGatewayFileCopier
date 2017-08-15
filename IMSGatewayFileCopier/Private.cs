c03d9af753e5a216ca6c8f8c5ffc8bf0131a9f92
DevMolasses <DevMolasses@gmail.com>
2017-08-15 09:19:59 -0500

    Added file to store username and password

---
+++ IMSGatewayFileCopier/Private.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMSGatewayFileCopier
+{
+    class Private
+    {
+        private string uN = "unsername";
+        private string pW = "password";
+        public string username { get { return uN; } set { } }
+        public string password { get { return pW; } set { } }
+
+        public Private() { }
+    }
+}
