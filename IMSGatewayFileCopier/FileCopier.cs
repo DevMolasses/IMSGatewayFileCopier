a8fa37d2eac60118e3365c7c9e77eeb1eb887006
DevMolasses <DevMolasses@gmail.com>
2017-08-17 13:46:57 -0500

    Added sub-directory creation and Console Reporting

--- IMSGatewayFileCopier/FileCopier.cs
+++ IMSGatewayFileCopier/FileCopier.cs
@@ -12,10 +12,8 @@ namespace IMSGatewayFileCopier
     {
         public FileCopier() { }
 
-        public static void CopyFile(string sourceFile, string sourceDirectory, string destinationDirectory)
+        public static void CopyFile(string sourceFile, string sourceDirectory, string destinationDirectory, bool newFile = false)
         {
-            string destinationFile = sourceFile.Replace(sourceDirectory, destinationDirectory);
-
             int tries = 0;
             int allowedTries = 100;
             bool fileCopied = false;
@@ -25,13 +23,28 @@ namespace IMSGatewayFileCopier
                 tries++;
                 try
                 {
-                    if (!File.Exists(destinationFile))
+                    string destinationExt = Path.GetFileName(sourceFile).Substring(0, 25);
+                    destinationDirectory = destinationDirectory + @"\" + destinationExt;
+                    string destinationFile = sourceFile.Replace(sourceDirectory, destinationDirectory);
+
+                    if (!Directory.Exists(destinationDirectory)) Directory.CreateDirectory(destinationDirectory);
+
+                    if (!newFile)
+                    {
+                        if (!File.Exists(destinationFile))
+                        {
+                            File.Copy(sourceFile, destinationFile);
+                            fileCopied = true;
+                            break;
+                        }
+                        else fileSkipped = true;
+                    }
+                    else
                     {
                         File.Copy(sourceFile, destinationFile);
                         fileCopied = true;
                         break;
                     }
-                    else fileSkipped = true;
                 }
                 catch
                 {
@@ -39,10 +52,13 @@ namespace IMSGatewayFileCopier
                 }
             }
             if (!fileCopied && !fileSkipped) Console.WriteLine(DateTime.Now + " - Unable to Copy " + Path.GetFileName(sourceFile));
+            if (fileCopied) Console.WriteLine(DateTime.Now + " - Copied " + Path.GetFileName(sourceFile));
+            if (fileSkipped) Console.WriteLine(DateTime.Now + " - Skipped " + Path.GetFileName(sourceFile));
         }
 
         public static void CopyAllFiles(string sourceDirectory, string destinationDirectory)
         {
+            DateTime start = DateTime.Now;
             string[] sourceFiles = Directory.GetFiles(sourceDirectory);
 
             for (int i = 0; i < sourceFiles.Length; i++)
@@ -50,7 +66,10 @@ namespace IMSGatewayFileCopier
                 string sourceFile = sourceFiles[i];
                 CopyFile(sourceFile, sourceDirectory, destinationDirectory);
             }
-            Console.WriteLine(DateTime.Now + " - Finished copying {0} files", sourceFiles.Length);
+            DateTime finish = DateTime.Now;
+            TimeSpan timeLapsed = finish.Subtract(start);
+            Console.WriteLine(finish + " - Finished copying {0} files", sourceFiles.Length);
+            Console.WriteLine(finish + " - TimeLapsed: {0}", timeLapsed);
         }
     }
 }
